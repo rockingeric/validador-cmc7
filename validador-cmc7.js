74c10d5cb8a2475bb675d60fdb1e737c67309c2b
Eric Martins <3ricm2@gmail.com>
2013-11-20 18:24:32 -0200

    prettyfing

--- validador-cmc7.js
+++ validador-cmc7.js
@@ -1,8 +1,8 @@
-function mod(dividendo, divisor){
-    return Math.round(dividendo - (Math.floor(dividendo/divisor)*divisor));
+function mod(dividend, divisor){
+    return Math.round(dividend - (Math.floor(dividend/divisor)*divisor));
 }
 
-function replaceAll(string, token, newtoken) {
+function replace_all(string, token, newtoken) {
     while (string.indexOf(token) != -1) {
         string = string.replace(token, newtoken);
     }
@@ -10,53 +10,53 @@ function replaceAll(string, token, newtoken) {
 }
 
 // validador de cmc7
-function valida_cmc7(campo){
-    campo      = replaceAll(campo, " ", "");
-    var campo1 = campo.substr(0,7);
-    var campo2 = campo.substr(8,10);
-    var campo3 = campo.substr(19,10);
+function check_cmc7(typedValue){
+    typedValue      = replace_all(typedValue, " ", "");
+    var typedValue1 = typedValue.substr(0,7);
+    var typedValue2 = typedValue.substr(8,10);
+    var typedValue3 = typedValue.substr(19,10);
 
-    var dv1 = campo.substr(7,1);
-    var dv2 = campo.substr(18,1);
-    var dv3 = campo.substr(29,1);
+    var dv1 = typedValue.substr(7,1);
+    var dv2 = typedValue.substr(18,1);
+    var dv3 = typedValue.substr(29,1);
 
-    var is_valid = true;
+    var isValid = true;
 
-    if(campo.trim()){
-        if (modulo10(campo1) != dv2) {
-            is_valid = false;
-        } else if (modulo10(campo2) != dv1){
-            is_valid = false;
-        } else if (modulo10(campo3) != dv3){
-            is_valid = false;
+    if(typedValue.trim()){
+        if (modulo10(typedValue1) != dv2) {
+            isValid = false;
+        } else if (modulo10(typedValue2) != dv1){
+            isValid = false;
+        } else if (modulo10(typedValue3) != dv3){
+            isValid = false;
         }
     } else {
-        is_valid = false;
+        isValid = false;
     }
-    return is_valid;
+    return isValid;
 }
 
 
 
 function modulo10(str){
-    var tamanho = str.length - 1;
-    var soma = 0;
-    var peso = 2;
+    var size = str.length - 1;
+    var result = 0;
+    var weight = 2;
 
-    for (var i = tamanho; i >= 0; i--) {
-        total = str.substr(i, 1) * peso;
+    for (var i = size; i >= 0; i--) {
+        total = str.substr(i, 1) * weight;
         if (total > 9) {
-            soma = soma + 1 + (total - 10);
+            result = result + 1 + (total - 10);
         } else {
-            soma = soma + total;
+            result = result + total;
         }
-        if (peso == 1) {
-            peso = 2;
+        if (weight == 1) {
+            weight = 2;
         } else {
-            peso = 1;
+            weight = 1;
         }
     }
-    var dv = 10 - mod(soma, 10);
+    var dv = 10 - mod(result, 10);
     if (dv == 10) {
         dv = 0;
     }
