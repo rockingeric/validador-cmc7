059169b811526d3b56dd59aac84434f7b0d31108
Eric Martins <3ricm2@gmail.com>
2013-12-10 01:45:37 -0200

    total redesign of the tool

--- validador-cmc7.js
+++ validador-cmc7.js
@@ -1,64 +1,71 @@
-function mod(dividend, divisor){
-    return Math.round(dividend - (Math.floor(dividend/divisor)*divisor));
-}
+;(function(context){
+  
 
-function replace_all(string, token, newtoken) {
-    while (string.indexOf(token) != -1) {
-        string = string.replace(token, newtoken);
-    }
-    return string;
-}
-
-// validador de cmc7
-function check_cmc7(typedValue){
-    typedValue      = replace_all(typedValue, " ", "");
-    var typedValue1 = typedValue.substr(0,7);
-    var typedValue2 = typedValue.substr(8,10);
-    var typedValue3 = typedValue.substr(19,10);
-
-    var dv1 = typedValue.substr(7,1);
-    var dv2 = typedValue.substr(18,1);
-    var dv3 = typedValue.substr(29,1);
-
-    var isValid = true;
-
-    if(typedValue.trim()){
-        if (modulo10(typedValue1) != dv2) {
-            isValid = false;
-        } else if (modulo10(typedValue2) != dv1){
-            isValid = false;
-        } else if (modulo10(typedValue3) != dv3){
-            isValid = false;
+  var Cmc7Validator = {
+    validate : function (typedValue){
+        typedValue      = typedValue.replace(/\s/g, "");
+        if(!typedValue){
+          return false;
         }
-    } else {
-        isValid = false;
-    }
-    return isValid;
-}
 
+        var pieces = {
+          firstPiece  : typedValue.substr(0,7)
+          , secondPiece : typedValue.substr(8,10)
+          , thirdPiece  : typedValue.substr(19,10)
+        };
+        
 
+        var digits = {
+          firstDigit : parseInt(typedValue.substr(7,1))
+          , secondDigit :  parseInt(typedValue.substr(18,1))
+          , thirdDigit : parseInt(typedValue.substr(29,1))
+        };
 
-function modulo10(str){
-    var size = str.length - 1;
-    var result = 0;
-    var weight = 2;
+        var calculatedDigits = {
+          firstDigit : this.modulo10(pieces.firstPiece)
+          , secondDigit :  this.modulo10(pieces.secondPiece)
+          , thirdDigit : this.modulo10(pieces.thirdPiece)
+        };
 
-    for (var i = size; i >= 0; i--) {
-        total = str.substr(i, 1) * weight;
-        if (total > 9) {
-            result = result + 1 + (total - 10);
-        } else {
-            result = result + total;
+        if ( (calculatedDigits.secondDigit != digits.firstDigit) 
+          || (calculatedDigits.firstDigit != digits.secondDigit)
+          || (calculatedDigits.thirdDigit != digits.thirdDigit) ) {
+            return false;
         }
-        if (weight == 1) {
-            weight = 2;
-        } else {
-            weight = 1;
+        return true;
+    }
+    , modulo10 : function(str){
+        var size = str.length - 1;
+        var result = 0;
+        var weight = 2;
+
+        for (var i = size; i >= 0; i--) {
+
+            total = str.substr(i, 1) * weight;
+
+            if (total > 9) {
+                result = result + 1 + (total - 10);
+            } else {
+                result = result + total;
+            }
+            if (weight == 1) {
+                weight = 2;
+            } else {
+                weight = 1;
+            }
         }
+        var dv = 10 - this.mod(result, 10);
+        if (dv == 10) {
+            dv = 0;
+        }
+        return dv;
     }
-    var dv = 10 - mod(result, 10);
-    if (dv == 10) {
-        dv = 0;
+    , mod : function(dividend, divisor){
+        return Math.round(dividend - (Math.floor(dividend/divisor)*divisor));
     }
-    return dv;
-}
+
+  }
+
+  // Expose the module function.
+  context.Cmc7Validator = Cmc7Validator;
+})(window);
